8382c49ce7fd65a72341fe803e1f5251a9407fa2
giangleee <giangleee>
2022-12-14 21:15:25 +0700

    Update W7

--- Week7/tcp_client.c
+++ Week7/tcp_client.c
@@ -7,54 +7,48 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define SERVER_ADDR "127.0.0.1"
-#define SERVER_PORT 5550
 #define BUFF_SIZE 1024
-#define MAX_CHAR 100
 
 int main(int argc, char *argv[])
 {
 	int client_sock;
 	char buff[BUFF_SIZE + 1];
-	struct sockaddr_in server_addr; /* server's address information */
-	int msg_len, bytes_sent, bytes_received;
-	char docs_path[MAX_CHAR];
+	struct sockaddr_in server_addr_info; /* server's address information */
+	int bytes_sent, bytes_received;
+	char docs_path[BUFF_SIZE + 1], server_addr[BUFF_SIZE + 1];
+	int server_port;
 
 	// Step 0: Get data from command
-	if (argc != 2)
+	if (argc != 4)
 	{
 		printf("Wrong command!\n");
 		exit(0);
 	}
 	else
 	{
-		strcpy(docs_path, argv[1]);
+		strcpy(server_addr, argv[1]);
+		server_port = atoi(argv[2]);
+		strcpy(docs_path, argv[3]);
 	}
 
 	// Step 1: Construct socket
 	client_sock = socket(AF_INET, SOCK_STREAM, 0);
 
 	// Step 2: Specify server address
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(SERVER_PORT);
-	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+	server_addr_info.sin_family = AF_INET;
+	server_addr_info.sin_port = htons(server_port);
+	server_addr_info.sin_addr.s_addr = inet_addr(server_addr);
 
 	// Step 3: Request to connect server
-	if (connect(client_sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) < 0)
+	if (connect(client_sock, (struct sockaddr *)&server_addr_info, sizeof(struct sockaddr)) < 0)
 	{
 		printf("\nError!Can not connect to sever! Client exit imediately! ");
 		return 0;
 	}
 
 	// Step 4: Communicate with server
-
-	// send message
-	printf("\nInsert string to send:");
-	memset(buff, '\0', (strlen(buff) + 1));
-	fgets(buff, BUFF_SIZE, stdin);
-	msg_len = strlen(buff);
-
-	bytes_sent = send(client_sock, buff, msg_len, 0);
+	// send file text path
+	bytes_sent = send(client_sock, docs_path, strlen(docs_path), 0);
 	if (bytes_sent < 0)
 		perror("\nError: ");
 
@@ -66,7 +60,17 @@ int main(int argc, char *argv[])
 		printf("Connection closed.\n");
 
 	buff[bytes_received] = '\0';
-	printf("Reply from server: %s", buff);
+	printf("New file's path: %s\n", buff);
+
+	// receive echo reply
+	bytes_received = recv(client_sock, buff, BUFF_SIZE, 0);
+	if (bytes_received < 0)
+		perror("\nError: ");
+	else if (bytes_received == 0)
+		printf("Connection closed.\n");
+
+	buff[bytes_received] = '\0';
+	printf("Number of bytes: %s\n", buff);
 
 	// Step 4: Close socket
 	close(client_sock);
--- Week7/tcp_server.c
+++ Week7/tcp_server.c
@@ -11,7 +11,6 @@
 #include <signal.h>
 #include <arpa/inet.h>
 
-#define PORT 5550
 #define BACKLOG 20
 #define BUFF_SIZE 1024
 #define TRUE 1
@@ -26,7 +25,17 @@ void sig_chld(int signo);
  */
 void echo(int sockfd);
 
-int main()
+/**
+ * Reverse string in place 
+*/
+void reverse(char s[]);
+
+/** 
+ * Convert n to characters in s 
+*/
+void itoa(int n, char s[]);
+
+int main(int argc, char *argv[])
 {
 
 	int listen_sock, conn_sock; /* file descriptors */
@@ -34,11 +43,23 @@ int main()
 	struct sockaddr_in client;	/* client's address information */
 	pid_t pid;
 	int sin_size;
+	int PORT;
 
 	if ((listen_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 	{ /* calls socket() */
 		printf("socket() error\n");
 		return 0;
+	}
+
+	// Step 0: Get data from command
+	if (argc != 2)
+	{
+		printf("Wrong command!\n");
+		exit(0);
+	}
+	else
+	{
+		PORT = atoi(argv[1]);
 	}
 
 	bzero(&server, sizeof(server));
@@ -115,9 +136,116 @@ void echo(int sockfd)
 	else if (bytes_received == 0)
 		printf("Connection closed.");
 
-	bytes_sent = send(sockfd, buff, bytes_received, 0); /* echo to the client */
+	char *token = malloc(BUFF_SIZE * sizeof(char));
+	strcpy(token, buff);
+	char docs_path_split_token[20][BUFF_SIZE + 1];
+	int count = 0;
+
+	token = strtok(token, "/");
+
+	while (token != NULL)
+	{
+		strcpy(docs_path_split_token[count], token);
+		token = strtok(NULL, "/");
+		count++;
+	}
+
+	char file[strlen(docs_path_split_token[count - 1]) + 1];
+	strcpy(file, docs_path_split_token[count - 1]);
+	char *file_name = strtok(file, ".");
+
+	int i = 0;
+	while (file[i] != '\0')
+	{
+		if (file[i] >= 'a' && file[i] <= 'z')
+		{
+			file[i] = file[i] - 32;
+		}
+		i++;
+	}
+	strcat(file, ".txt");
+	char *newPath = malloc(BUFF_SIZE * sizeof(char));
+	strcat(newPath, "/");
+
+	for (int i = 0; i < count - 1; i++)
+	{
+		strcat(newPath, docs_path_split_token[i]);
+		strcat(newPath, "/");
+	}
+	strcat(newPath, file);
+
+	FILE *ptr_in, *ptr_out;
+	ptr_in = fopen(buff, "r+");
+	ptr_out = fopen(newPath, "w");
+	char ch;
+
+	if (ptr_in == NULL)
+	{
+		perror("\nError: \n");
+	}
+	else
+	{
+		while (ch != EOF)
+		{
+			ch = fgetc(ptr_in);
+			if ((ch >= 'a') && (ch <= 'z'))
+			{
+				ch = ch - 32;
+				fputc(ch, ptr_out);
+			}
+			else
+				fputc(ch, ptr_out);
+		}
+	}
+
+	fseek(ptr_out, 0L, SEEK_END);
+
+	// calculating the size of the file
+	long int res = ftell(ptr_out);
+	printf("%ld", res);
+
+	fclose(ptr_in);
+	fclose(ptr_out);
+
+	bytes_sent = send(sockfd, newPath, strlen(newPath), 0); /* echo to the client */
+	if (bytes_sent < 0)
+		perror("\nError: ");
+	char sres[BUFF_SIZE + 1];
+	// 	convert res to string [buf]
+	itoa(res, sres);
+	bytes_sent = send(sockfd, sres, strlen(sres), 0); /* echo to the client */
 	if (bytes_sent < 0)
 		perror("\nError: ");
 
 	close(sockfd);
+}
+
+void reverse(char s[])
+{
+	int i, j;
+	char c;
+
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
+}
+
+void itoa(int n, char s[])
+{
+	int i, sign;
+
+	if ((sign = n) < 0) /* record sign */
+		n = -n;			/* make n positive */
+	i = 0;
+	do
+	{						   /* generate digits in reverse order */
+		s[i++] = n % 10 + '0'; /* get next digit */
+	} while ((n /= 10) > 0);   /* delete it */
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	reverse(s);
 }
